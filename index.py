e84fa8f4b634410e3cd427dfcb4c3543af21b3e3
Tan Tran <tankimtran@gmail.com>
2013-08-18 19:12:29 -0700

    first redirect w/ query string

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ import jinja2
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), auto_reload = True)
 
 def render_str(template, **params):
     t = jinja_env.get_template(template)
@@ -45,43 +45,47 @@ class ResumePage(webapp2.RequestHandler):
 	def post(self):
 		courses = self.request.get('courses-text')
 		
-		## back when I thought courses were going to be integer-indexed
-##		courses_split = courses.split(',')
-##		
-##		course_numbers = []
-##		for course in courses_split:
-##			course_number = int(course)
-##			course_numbers.append(course_number)
+		# For each course entered, check if it's valid.
+		# If valid, enter into user
+		courses_added = []
+		courses_not_added = []
+		
+		student_key = db.Key.from_path('Student', "tktran", parent = site_key())
+		student = db.get(student_key)
+		courses_list = student.student_courses
 		
 		for course in courses.split(','):
 			key = db.Key.from_path('Course', course, parent=site_key())
 			allowed_course = db.get(key)
 			
 			if allowed_course:
-				self.error(404)
-				return
-				
-		## now add those courses to the student. here, just to me.
-		## haven't figured out key vs. username distinction yet.
+				courses_added.append(course)
+				courses_list.append(key)
+			else:
+				courses_not_added.append(course)
+		
+		message = "These courses were added: " + ', '.join(courses_added)
 
 		# jury rigged to add new student to db
 		# 1. build the Course, which has name, description, importance
-		course = Course(key_name = "test_course", parent = site_key(),
-			name = "CECS TEST", description = "Long desc of course", importance = 50)
-		course.put()
-		key_to_course = db.Key.from_path('Course', "222", parent = site_key())
-		# 2. build StudentCourse, which has Course and Rating
-		new_student_course = StudentCourse(key_name = "test_sc", parent = site_key(),
-			course = key_to_course, rating = 50)
-		new_student_course.put()
-		key_to_nsc = db.Key.from_path('StudentCourse', 'test_sc', parent = site_key())
-		# 3. Build Student, which has username and student_courses (List of keys to SCs)
-		new_student_courses_list = [key_to_nsc]
-		new_student = Student(key_name = "tktran", parent = site_key(),
-			username = "Tan Tran", student_courses = new_student_courses_list)
-		new_student.put()
+	#	course = Course(key_name = "test_course", parent = site_key(),
+	#		name = "CECS TEST", description = "Long desc of course", importance = 50)
+	#	course.put()
+	#	key_to_course = db.Key.from_path('Course', "222", parent = site_key())
+	#	# 2. build StudentCourse, which has Course and Rating
+	#	new_student_course = StudentCourse(key_name = "test_sc", parent = site_key(),
+	#		course = key_to_course, rating = 50)
+	#	new_student_course.put()
+	#	key_to_nsc = db.Key.from_path('StudentCourse', 'test_sc', parent = site_key())
+	#	# 3. Build Student, which has username and student_courses (List of keys to SCs)
+	#	new_student_courses_list = [key_to_nsc]
+	#	new_student = Student(key_name = "tktran", parent = site_key(),
+	#		username = "Tan Tran", student_courses = new_student_courses_list)
+	#	new_student.put()
 		# success!
-		self.redirect('/success')
+		
+		self.redirect('/success?' + 'added=' + '+'.join(courses_added)
+			+ 'not=' + '+'.join(courses_not_added))
 
 class SuccessPage(webapp2.RequestHandler):
 	def write(self, *a, **kw):
@@ -94,7 +98,7 @@ class SuccessPage(webapp2.RequestHandler):
 		self.write(self.render_str(template, **kw))
 			
 	def get(self):
-		self.render('success.html', message = 'These courses were added:')
+		self.render('success.html')
 
 class Course(db.Model):
 	name = db.StringProperty(required = True)
